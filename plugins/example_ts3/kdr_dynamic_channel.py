a81c890cafe1ac6010b85ba2e9e266b521506588
Karl-Martin Minkner <kalle@kandru.de>
2015-04-05 22:08:18 +0200

    optimized kdr_dynamic_channel to NOT request channellist every loop and to NOT fail if we found a channel without number

--- plugins/example_ts3/kdr_dynamic_channel.py
+++ plugins/example_ts3/kdr_dynamic_channel.py
@@ -33,22 +33,18 @@ def setup(ts3base):
     # get core TS3channel
     core_TS3channel = base.get_class('core.TS3channel')
     # register callbacks
-    base.register_callback(name, 'ts3.clientjoined', clientjoin)
     base.register_callback(name, 'ts3.loop', loop)
-
-def clientjoin(client):
-    print('client joined')
-    print(client)
 
 def loop(self):
     global channellist, core_TS3channel
-    channellist = core_TS3channel.channellist()
     channelproof()
 
 def channelproof():
     global config, channellist, core_TS3channel, nextProof
     # if we can proof now or just have to wait
     if nextProof <= time.time():
+        # get channel list
+        channellist = core_TS3channel.channellist()
         # for each config entry
         for key in config:
             # set counter to 0 (for empty channel)
@@ -63,35 +59,54 @@ def channelproof():
                 for key2 in channellist:
                     # if we found a subchannel from parent
                     if int(channellist[key2]['pid']) == config[key]['parent']:
-                        channelexists[channellist[key2]['channel_name'].split("#")[1]] = channellist[key2]['cid']
-                        # just count them if they are empty
-                        if int(channellist[key2]['total_clients']) == 0:
-                            numechannel = numechannel + 1
-                            # delete them if we got more then one empty channel
-                            if numechannel > 1:
-                                core_TS3channel.channeldelete(channellist[key2]['cid'])
-                                numechannel = numechannel - 1
-                        else:
-                            # count not empty channel
-                            numchannel = numchannel + 1
+                        if '#' in channellist[key2]['channel_name']:
+                            channelexists[channellist[key2]['channel_name'].split("#")[1]] = channellist[key2]['cid']
+                            # just count them if they are empty
+                            if int(channellist[key2]['total_clients']) == 0:
+                                numechannel = numechannel + 1
+                                # delete them if we got more then one empty channel
+                                if numechannel > 1:
+                                    core_TS3channel.channeldelete(channellist[key2]['cid'])
+                                    numechannel = numechannel - 1
+                            else:
+                                # count not empty channel
+                                numchannel = numchannel + 1
                 # if we do not found an empty channel we need to create one :)
                 if numechannel == 0:
                     print('no empty channel, created one')
                     # set channel properties
-                    propertiesdict = {'channel_codec_quality': 10, 'channel_flag_maxclients_unlimited': 1, 'channel_flag_permanent': 1, 'cpid': config[key]['parent']}
+                    propertiesdict = {'i_channel_needed_join_power': 10, 'channel_codec_quality': 10, 'channel_flag_maxclients_unlimited': 1, 'channel_flag_permanent': 1, 'cpid': config[key]['parent']}
                     propertiesdict.update(config[key]['channel_properties'])
                     # set channel name
                     current = 1
                     channelname = config[key]['subchan_name'].replace('[COUNT]', str(current))
+                    # find empty channel ID
                     while 1:
+                        # and only proceed if we found an empty channel ID
                         if str(current) not in channelexists.keys():
+                            # set channel name to empty ID
                             channelname = config[key]['subchan_name'].replace('[COUNT]', str(current))
+                            # if the channel before exist (it must normally exist - or the new channel will be the first channel)
                             if str(current-1) in channelexists.keys():
+                                # create the new channel at the right place
                                 propertiesdict['channel_order'] = int(channelexists[str(current-1)])
                             else:
+                                # create the new channel at the beginning
                                 propertiesdict['channel_order'] = 0
                             break
                         current = current + 1
                     # create channel
                     core_TS3channel.channelcreate(channelname, propertiesdict)
+                    numechannel = numechannel + 1
+                if numechannel == 1 and numchannel == 0:
+                    for key2 in channellist:
+                        # if we found the empty subchannel from parent
+                        if int(channellist[key2]['pid']) == config[key]['parent']:
+                            if channellist[key2]['channel_name'][-2:] != '#1':
+                                channelname = config[key]['subchan_name'].replace('[COUNT]', str(1))
+                                # set channel properties
+                                propertiesdict = {'channel_codec_quality': 10, 'channel_flag_maxclients_unlimited': 1, 'channel_flag_permanent': 1, 'cpid': config[key]['parent']}
+                                propertiesdict.update(config[key]['channel_properties'])
+                                core_TS3channel.channeldelete(key2)
+                                core_TS3channel.channelcreate(channelname, propertiesdict)
         nextProof = time.time() + nextInterval
