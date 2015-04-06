107fe8e9e08052fa5a1750badca4b7381b2a638f
Jon-Mailes Graeffe <jm.graeffe@jonni.it>
2015-04-06 04:08:06 +0200

    outsourced the event parsing function

--- ts3tools.py
+++ ts3tools.py
@@ -26,3 +26,18 @@ class ts3tools:
     def set_nickname(socket, nickname):
         return socket.send(
             'clientupdate client_nickname=' + ts3tools.escape_text(nickname))
+    
+    def parse_raw_event(msg):
+        splitted = msg.split(" ")
+
+        new = {'ev_type': splitted[0]}
+        # because in the new dict the event name already exists, we remove it from the splitted list
+        splitted.pop(0)
+        # parsing arguments
+        for arg in splitted:
+            one = arg.split('=', 1)
+            if len(one) == 2:
+                new[ts3tools.unescape_text(one[0])] = ts3tools.unescape_text(one[1])
+            else:
+                new[ts3tools.unescape_text(one[0])] = None
+        return new
