3c4e3f08fba5406b4ddb49f6744fda7d43735e2f
Jon-Mailes Graeffe <jm.graeffe@jonni.it>
2015-04-06 04:22:24 +0200

    added new core plugin: core.TS3chat (chat and chat command handling)

---
+++ plugins/core_TS3chat.py
@@ -0,0 +1,94 @@
+from ts3tools import ts3tools
+
+"""
+core.TS3chat
+
+Provides basic text chat events and therefore, it implements a command handler.
+The command handler executes a command event if a chat message has a prefix (default is "!") defined in the chat configuration.
+Command events are pre-formatted for other plugins with arguments etc.
+"""
+
+name = 'core.TS3chat'
+base = None
+command_socket = None
+event_socket = None
+config = None
+
+def setup(ts3base):
+    global base
+    global command_socket
+    global event_socket
+    global chathelper
+    global config
+
+    base = ts3base
+    command_socket = base.get_command_socket()
+    event_socket = base.get_event_socket()
+
+    config = ts3tools.get_global_config(name)
+
+    chathelper = ChatHelper()
+    base.register_class(name, ChatHelper)
+
+    base.register_callback(name, 'ts3.receivedevent', event_receivedevent)
+    base.register_callback(name, 'ts3.clientjoined', event_clientjoined)
+
+def event_receivedevent(data):
+    event = ts3tools.parse_raw_event(data)
+
+    if (event["ev_type"] == 'notifytextmessage') and (event['invokerid'] != base.clid):
+        if event["targetmode"] == '1':
+            event.pop("ev_type", None)
+            # event --> chat_msg --> chat_cmd
+            chat_msg(event)
+
+def chat_msg(event):
+    if event['msg'].startswith(config['General']['command_prefix']):
+        chat_cmd(event)
+    else:
+        base.execute_callback('ts3.chat.msg', event)
+
+def chat_cmd(event):
+    cmd = {}
+    cmd['sender'] = {   'id': event['invokerid'],
+                        'uid': event['invokeruid'],
+                        'name': event['invokername']}
+    cmd['args'] = event['msg'].split(' ')
+    cmd['command'] = cmd['args'][0]
+    cmd['args'].pop(0)
+    
+    # delete first character (command_prefix)
+    base.execute_callback('ts3.chat.cmd.' + cmd['command'][1:], cmd)
+    # debug message
+    base.debprint('core_TS3chat: received chat command ' + cmd['command'] + ' from ' + cmd['sender']['name'] + ' with args ' + str(cmd['args']))
+
+def event_clientjoined(user):
+    # send welcome message, if enabled
+    if config['Welcome']['enabled'] == 'true':
+        if config['Welcome']['force_user'] != 'false':
+            ts3tools.set_nickname(base, config['Welcome']['force_user'] , True)
+        chathelper.send_pm(base, user["clid"], ts3tools.escape_text(config['Welcome']['message']), True)
+
+class ChatHelper:
+    def __init__(self):
+        pass
+
+    def send_pm(self, base, user, msg, socket=False):
+        """
+        Sends a private message to given user.
+        """
+        if socket == False:
+            return base.send_receive(
+               'sendtextmessage targetmode=1 msg=' + msg + ' target=' + user)
+        else:
+            return base.get_event_socket().send(
+                'sendtextmessage targetmode=1 msg=' + msg + ' target=' + user)
+
+    def send_sm(self, base, msg, socket=False):
+        """
+        Sends a server message (all users can see it in the server tab).
+        """
+        if socket == False:
+            return base.send_receive('sendtextmessage targetmode=3 msg=' + msg)
+        else:
+            return base.get_event_socket().send('sendtextmessage targetmode=3 msg=' + msg)
