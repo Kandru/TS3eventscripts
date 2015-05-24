34119dc6b9aedc9335c146950a5c65eb53606fb6
Karl-Martin Minkner <kalle@kandru.de>
2015-05-25 01:09:52 +0200

    updated jonni usertracker and eliminated a lot of bugs. Even optimized some things. Have a look at the changelog entry 2.

--- plugins/jonni_userTracker.py
+++ plugins/jonni_userTracker.py
@@ -6,6 +6,22 @@ Later there are some more tracked things like last visit, amount of time being o
 
 import time
 
+""" Changelog
+-------------
+2. kandru
+- fixed UPDATE statements. Should now only update one user, not all
+- fixed "answer_" variables. We should now use column names
+- added "ts3.clientleft"
+- added function "create_client"
+- added timestamp and columns "ts_lastonline", "ts_lastchange", "ts_insert"
+- added column "total_online" to measure total online time in seconds
+- added ts3 instance information
+- fixed typos
+1. j0nnib0y - initial commit
+"""
+
+
+
 name = 'jonni.userTracker'
 
 base = None
@@ -18,6 +34,7 @@ def setup(ts3base):
 
     base.register_callback(name, 'ts3.start', startup)
     base.register_callback(name, 'ts3.clientjoined', event_clientjoined)
+    base.register_callback(name, 'ts3.clientleft', event_clientleft)
     base.register_class(name, UserTracker)
 
 
@@ -26,7 +43,7 @@ def startup(bla):
     db = base.get_class('core.TS3db')
 
     db.create_table(
-        name, [['uid', 'VARCHAR(30)'], ['dbid', 'INT'], ['clid', 'INT'], ['last_name', 'TEXT'], ['ts_lastchange', 'INT'], ['ts_insert', 'INT']], 'users')
+        name, [['instance', 'VARCHAR(255)'], ['uid', 'VARCHAR(30)'], ['dbid', 'INT'], ['clid', 'INT'], ['last_name', 'TEXT'], ['total_online', 'INT'], ['ts_lastonline', 'INT'], ['ts_lastchange', 'INT'], ['ts_insert', 'INT']], 'users')
 
 def event_clientjoined(values):
     uid = values['client_unique_identifier']
@@ -35,27 +52,42 @@ def event_clientjoined(values):
     clid = values['clid']
     timestamp = str(time.time())
 
-    if db.execute('SELECT * FROM `' + db.get_table_name(name, 'users') + '` WHERE `uid` = "' + uid + '";'):
+    if db.execute('SELECT * FROM `' + db.get_table_name(name, 'users') + '` WHERE `uid` = "' + uid + '" AND `instance` = "' + base.identifier + '";'):
         answer = db.fetch_one()
         if answer is None:
-            db.execute('INSERT INTO `' + db.get_table_name(name, 'users') + '` (`uid`, `dbid`, `last_name`,`ts_lastchange`,`ts_insert`) VALUES ("' + uid + '", ' + dbid + ', "' + last_name + '", "' + timestamp + '", "' + timestamp + '");')
-            db.fetch_one()
+            create_client(values)
         else:
-            answer_dbid = answer[2]
-            answer_clid = answer[3]
-            answer_name = answer[4]
-            if answer_dbid != dbid:
-                db.execute('UPDATE `' + db.get_table_name(name, 'users') + '` SET dbid = "' + dbid + '", ts_lastchange = "' + timestamp + '";')
+            # set lastonline timestamp to now
+            db.execute('UPDATE `' + db.get_table_name(name, 'users') + '` SET ts_lastonline = "' + timestamp + '", ts_lastchange = "' + timestamp + '" WHERE `uid` = "' + uid + '" AND `instance` = "' + base.identifier + '";')
+            db.fetch_one()
+            # if something has changed since last connect
+            if answer['dbid'] != dbid:
+                db.execute('UPDATE `' + db.get_table_name(name, 'users') + '` SET dbid = "' + dbid + '", ts_lastchange = "' + timestamp + '" WHERE `uid` = "' + uid + '" AND `instance` = "' + base.identifier + '";')
                 db.fetch_one()
-            if answer_name != last_name:
-                db.execute('UPDATE `' + db.get_table_name(name, 'users') + '` SET last_name = "' + last_name + '", ts_lastchange = "' + timestamp + '";')
+            if answer['last_name'] != last_name:
+                db.execute('UPDATE `' + db.get_table_name(name, 'users') + '` SET last_name = "' + last_name + '", ts_lastchange = "' + timestamp + '" WHERE `uid` = "' + uid + '" AND `instance` = "' + base.identifier + '";')
                 db.fetch_one()
-            if answer_clid != clid:
-                db.execute('UPDATE `' + db.get_table_name(name, 'users') + '` SET clid = "' + clid + '", ts_lastchange = "' + timestamp + '";')
+            if answer['clid'] != clid:
+                db.execute('UPDATE `' + db.get_table_name(name, 'users') + '` SET clid = "' + clid + '", ts_lastchange = "' + timestamp + '" WHERE `uid` = "' + uid + '" AND `instance` = "' + base.identifier + '";')
     # [TODO: more details, statistics, ...]
 
-def event_clientleft():
-    pass
+def event_clientleft(values):
+    clid = values['clid']
+    timestamp = str(time.time())
+    # set new online time after user has left the server
+    if db.execute('SELECT * FROM `' + db.get_table_name(name, 'users') + '` WHERE `clid` = "' + clid + '" AND `instance` = "' + base.identifier + '";'):
+        answer = db.fetch_one()
+        if answer is not None:
+            db.execute('UPDATE `' + db.get_table_name(name, 'users') + '` SET total_online = total_online + "' + str(round(float(timestamp) - float(answer['ts_lastonline']))) + '", ts_lastchange = "' + timestamp + '", clid = "' + clid + '" WHERE `clid` = "' + clid + '" AND `instance` = "' + base.identifier + '";')
+
+def create_client(values):
+    uid = values['client_unique_identifier']
+    last_name = values['client_nickname']
+    dbid = values['client_database_id']
+    clid = values['clid']
+    timestamp = str(time.time())
+    db.execute('INSERT INTO `' + db.get_table_name(name, 'users') + '` (`instance`, `uid`, `dbid`, `clid`, `last_name`,`total_online`,`ts_lastonline`,`ts_lastchange`,`ts_insert`) VALUES ("' + base.identifier + '", "' + uid + '", "' + dbid + '", "' + clid + '", "' + last_name + '", "0", "' + timestamp + '", "' + timestamp + '", "' + timestamp + '");')
+    db.fetch_one()
 
 class UserTracker:
 
